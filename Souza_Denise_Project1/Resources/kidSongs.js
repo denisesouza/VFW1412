13b8a39106a9d50fadd9b6da7e1256420a3c4584
D Souza <drsouza@fullsail.edu>
2014-12-02 15:13:05 -0500

    Added function and function calls for the click and eventListeners

--- Souza_Denise_Project1/Resources/kidSongs.js
+++ Souza_Denise_Project1/Resources/kidSongs.js
@@ -2,4 +2,31 @@
  * @author Denise Souza
  */
 
-var songs = [""]
+var songs = ["Wheels on the Bus", "Twinkle Twinkle Little Star", "Humpty Dumpty", "Itsy Bitsy Spider", "Baa Baa Black Sheep", "Ring around the Rosie"];
+var length = myArray.length;
+var i = -1;
+var margin = 20;
+
+
+//function for displaying text by clicking the next button
+var displayNext = function(){
+	if(i === length -1){
+		i = -1;
+	};
+	mainMessage.text = myArray[i];
+};
+
+
+
+//function for displaying text by clicking the previous button
+var displayPrevious = function(){
+	if(i === length -1){
+		i = -1;
+	};
+	mainMessage.text = myArray[i];
+};
+
+
+//main code
+nextButton.addEventListener("click", displayNext);
+previousButton.addEventListener("click", displayPrevious);
